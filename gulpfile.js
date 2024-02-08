1c8b399e1bba24a567235ed9a3e4e27cf4f62801
abdul2946 <aazim3510@gmail.com>
2024-02-08 16:35:05 +0530

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+// const browserSync = require("browser-sync")
+
+
+
+function compilesass() {
+	return gulp
+		.src("./build/scss/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest("./css/"));
+}
+
+function watchTask() {
+	gulp.watch("./build/scss/*.scss", compilesass);
+}
+
+exports.default = gulp.series(compilesass,watchTask)
